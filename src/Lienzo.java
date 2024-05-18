e7f0e0d525ee796c3d26a84d0418e71410ae1eb0
Juan_F <juan.e.franco@correounivalle.edu.co>
2024-05-17 22:03:54 -0500

    se arreglan algunos errores en el archivo lienzo

--- src/Lienzo.java
+++ src/Lienzo.java
@@ -1,81 +1,61 @@
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.util.Random;
+import javax.swing.JPanel;
 
+class Lienzo extends JPanel implements MouseListener, MouseMotionListener {
+    private int x1, y1, x2, y2;
+    private Color lineColor = Color.BLACK; // Establece el color de la línea
 
-import javax.swing.JFrame;
-
-class Lienzo extends JFrame implements MouseListener, MouseMotionListener{
-    
-    private int x1, y1, x2, y2 ;
-    Container contenedor;
-
-    public Lienzo(){
-
-        contenedor = getContentPane();
+    public Lienzo() {
         addMouseListener(this);
         addMouseMotionListener(this);
-        setBounds(50, 50, 600,500);
-        setTitle("Dibujo Canvas");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
     }
 
-    
     @Override
     public void mouseMoved(MouseEvent e) {
-        
+        // No implementation needed
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        
+        // No implementation needed
     }
-    public void paint(Graphics g){
 
-        Random random = new Random();
-        int red = random.nextInt(250);
-        int green = random.nextInt(250);
-        int blue = random.nextInt(250);
-        g.setColor(new Color(red, green, blue));
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(lineColor);
         g.drawLine(x1, y1, x2, y2);
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        // TODO Auto-generated method stub
-
         x1 = e.getX();
         y1 = e.getY();
-    
     }
-    public void mouseDragged(MouseEvent e) {
-        // TODO Auto-generated method stub
-
-        x2 = e.getX();
-        y2 = e.getY();
-       
-    }
-
 
     @Override
-    public void mouseReleased(MouseEvent e) {
-       
+    public void mouseDragged(MouseEvent e) {
+        x2 = e.getX();
+        y2 = e.getY();
         repaint();
     }
 
     @Override
+    public void mouseReleased(MouseEvent e) {
+        // No implementation needed
+    }
+
+    @Override
     public void mouseEntered(MouseEvent e) {
-       
+        // No implementation needed
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        
-    } 
-
+        // No implementation needed
+    }
 }
